7d7405931d3af2269b487cf27ecccf0df803cb88
Xinwei <usami@mail.ustc.edu.cn>
2023-04-12 11:40:01 -0500

    Update DiceSGD

--- DiceSGD/model_utils.py
+++ DiceSGD/model_utils.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
+import math
 
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
@@ -66,6 +67,40 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
 
+class CifarCnn(nn.Module):
+    def __init__(self, input_shape, out_dim):
+        super(CifarCnn, self).__init__()
+        self.conv1 = nn.Conv2d(input_shape[0], 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.fc1 = nn.Linear(64*5*5, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, out_dim)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                stdv = 1. / math.sqrt(m.weight.size(1))
+                m.weight.data.uniform_(-stdv, stdv)
+                if m.bias is not None:
+                    m.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+
 
 def create_resnet(num_classes = 10):
     return ResNet(BasicBlock, [3,4,3], num_classes)
+
+def create_cnn(num_classes = 10, size=224):
+    return CifarCnn(input_shape=[size], out_dim=num_classes)
--- DiceSGD/optimizers.py
+++ DiceSGD/optimizers.py
@@ -2,12 +2,13 @@ import torch
 import math
 from torch.optim import Optimizer
 from fastDP import PrivacyEngine
+from opacus.accountants.utils import get_noise_multiplier
 
-PRINT_FREQ = 100
-SPLIT_FACT = 1.4142
+PRINT_FREQ = 25
+SPLIT_FACT = 2
 
-def ClipSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
-    # optimizer_useless=torch.optim.SGD(model.parameters(), lr=lr) 
+def ClipSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr, logger):
+    optimizer_useless=torch.optim.SGD(model.parameters(), lr=lr) 
     privacy_engine = PrivacyEngine(
         model,
         batch_size=batch,
@@ -17,11 +18,11 @@ def ClipSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
         max_grad_norm= C,
         loss_reduction= 'sum',
         clipping_fn='Abadi')
-    # privacy_engine.attach(optimizer_useless)
+    privacy_engine.attach(optimizer_useless)
     acc_step = batch//minibatch
 
-    # optimizer=torch.optim.Adam(model.parameters(), lr = lr)
-    optimizer=torch.optim.SGD(model.parameters(), lr = lr)
+    optimizer=torch.optim.Adam(model.parameters(), lr = lr)
+    # optimizer=torch.optim.SGD(model.parameters(), lr = lr)
     # criterion = torch.nn.CrossEntropyLoss()
     criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.1,reduction='sum')
     model.to(device)
@@ -44,24 +45,32 @@ def ClipSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
             correct += predicted.eq(label).sum().item()
 
             if ((t + 1) % acc_step == 0) or ((t + 1) == len(train_dl)):
-                for param in model.parameters():
-                    param.grad=param.summed_clipped_grad/batch
-                    del param.summed_clipped_grad
+                # for param in model.parameters():
+                #     param.grad=param.summed_clipped_grad/batch
+                #     del param.summed_clipped_grad
+                for layer, param in enumerate(model.parameters()):
+                    if param.requires_grad:
+                        if hasattr(param, 'summed_clipped_grad') == False:
+                            print(layer)
+                            param.grad = param.grad/batch
+                        else:
+                            param.grad=param.summed_clipped_grad/batch
+                            del param.summed_clipped_grad
                 optimizer.step()
                 optimizer.zero_grad()
 
             if t==0 or (t+1)%PRINT_FREQ == 0 or ((t + 1) == len(train_dl)):
                 print('\rEpoch: ', E, ':', t+1, 'Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
                         % (train_loss/(t+1), 100.*correct/total, correct, total), end='')
-                correct = 0
-                total = 0
-        test(E, t, model, test_dl,device)
+                # correct = 0
+                # total = 0
+        test(E, t, model, test_dl, device, lr, logger)
     return model
 
-def EFSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
+def EFSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr, logger):
     C1 = C/SPLIT_FACT
     C2 = C/SPLIT_FACT
-    # optimizer_useless=torch.optim.SGD(model.parameters(), lr= lr) 
+    optimizer_useless=torch.optim.SGD(model.parameters(), lr= lr) 
     privacy_engine = PrivacyEngine(
         model,
         batch_size=batch,
@@ -71,11 +80,11 @@ def EFSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
         max_grad_norm= C1,
         loss_reduction= 'sum',
         clipping_fn='Abadi')
-    # privacy_engine.attach(optimizer_useless)
+    privacy_engine.attach(optimizer_useless)
     acc_step = batch//minibatch
 
-    # optimizer=torch.optim.Adam(model.parameters(), lr = lr)
-    optimizer=torch.optim.SGD(model.parameters(), lr = lr)
+    optimizer=torch.optim.Adam(model.parameters(), lr = lr)
+    # optimizer=torch.optim.SGD(model.parameters(), lr = lr)
     criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.1,reduction='sum')
     model.to(device)
     model.train()
@@ -100,38 +109,43 @@ def EFSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
             if ((t + 1) % acc_step == 0) or ((t + 1) == len(train_dl)):
                 error_norms = []
                 for param in model.parameters():
-                    if hasattr(param,'error'):
-                        first_minibatch = False
-                        error_norms.append(param.error.norm(2))
-                    else:
-                        # param.error = None
-                        first_minibatch = True
-                        error_norms.append(torch.tensor(0.))
-                error_norm = torch.stack(error_norms).norm(2) + 1e-6
-
+                    if param.requires_grad:
+                        if hasattr(param,'error'):
+                            first_minibatch = False
+                            error_norms.append(param.error.norm(2))
+                        else:
+                            # param.error = None
+                            first_minibatch = True
+                            error_norms.append(torch.tensor(0.))
+                error_norm = torch.stack(error_norms).norm(2)
+                if error_norm<1e-6:
+                    error_norm = 1e-6
+                # print(error_norm)
                 for param in model.parameters():
-                    grad_diff=(param.grad-param.summed_clipped_grad)/batch
-                    param.grad=param.summed_clipped_grad/batch
-                    del param.summed_clipped_grad
-                    if first_minibatch:
-                        param.error=grad_diff
-                    else:
-                        param.grad += param.error*torch.clamp_max(C2/error_norm,1.)
-                        param.error=param.error*(1-torch.clamp_max(C2/error_norm,1.))+grad_diff
-                    del grad_diff
+                    if param.requires_grad:
+                        grad_diff=(param.grad-param.summed_clipped_grad)/batch
+                        param.grad=param.summed_clipped_grad/batch
+                        del param.summed_clipped_grad
+                        if first_minibatch:
+                            param.error=grad_diff
+                        else:
+                            param.grad += param.error*torch.clamp_max(C2/error_norm,1.)
+                            # param.error=param.error*(1-torch.clamp_max(C2/error_norm,1.))+grad_diff
+                            param.error = grad_diff
+                        del grad_diff
                 optimizer.step()
                 optimizer.zero_grad()
 
             if t==0 or (t+1)%PRINT_FREQ == 0 or ((t + 1) == len(train_dl)):
                 print('\rEpoch: ', E, ':', t+1, 'Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                        % (train_loss/(t+1), 100.*correct/total, correct, total),, end='')
-                correct = 0
-                total = 0
-        test(E, t, model, test_dl,device)
+                        % (train_loss/(t+1), 100.*correct/total, correct, total), end='')
+                # correct = 0
+                # total = 0
+        test(E, t, model, test_dl,device, lr, logger)
     return model
 
-def DPSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
-    # optimizer_useless=torch.optim.SGD(model.parameters(), lr=lr) 
+def DPSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr, logger):
+    optimizer_useless=torch.optim.SGD(model.parameters(), lr=lr) 
     privacy_engine = PrivacyEngine(
         model,
         batch_size=batch,
@@ -141,11 +155,18 @@ def DPSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
         max_grad_norm= C,
         loss_reduction= 'sum',
         clipping_fn='Abadi')
-    # privacy_engine.attach(optimizer_useless)
+    privacy_engine.attach(optimizer_useless)
 
-    # optimizer=torch.optim.Adam(model.parameters(), lr = lr)
+    sigma = get_noise_multiplier(
+        target_delta=1e-5,
+        target_epsilon=2,
+        sample_rate=batch/50000,
+        epochs=epoch
+    )
+
+    optimizer=torch.optim.Adam(model.parameters(), lr = lr)
     # criterion = torch.nn.CrossEntropyLoss()
-    optimizer=torch.optim.SGD(model.parameters(), lr = lr)
+    # optimizer=torch.optim.SGD(model.parameters(), lr = lr)
     criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.1,reduction='sum')
     model.to(device)
     model.train()
@@ -169,28 +190,29 @@ def DPSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
 
             if ((t + 1) % acc_step == 0) or ((t + 1) == len(train_dl)):
                 for param in model.parameters():
-                    param.grad=param.summed_clipped_grad/batch + torch.normal(
-                        mean=0,
-                        std=privacy_engine.noise_multiplier * C / math.sqrt(batch),
-                        size=param.size(),
-                        device=device,
-                    )
-                    del param.summed_clipped_grad
+                    if param.requires_grad:
+                        param.grad=param.summed_clipped_grad/batch + torch.normal(
+                            mean=0,
+                            std=sigma * C,
+                            size=param.size(),
+                            device=device,
+                        )/batch
+                        del param.summed_clipped_grad
                 optimizer.step()
                 optimizer.zero_grad()
 
             if t==0 or (t+1)%PRINT_FREQ == 0 or ((t + 1) == len(train_dl)):
                 print('\rEpoch: ', E, ':', t+1, 'Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
                         % (train_loss/(t+1), 100.*correct/total, correct, total), end='')
-                correct = 0
-                total = 0
-        test(E, t, model, test_dl,device)
+                # correct = 0
+                # total = 0
+        test(E, t, model, test_dl,device,  lr, logger)
     return model
 
-def DiceSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
-    C1 = C/SPLIT_FACT
-    C2 = C/SPLIT_FACT
-    # optimizer_useless=torch.optim.SGD(model.parameters(), lr=lr) 
+def DiceSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr, logger):
+    C1 = C/(SPLIT_FACT*batch)
+    C2 = C/(SPLIT_FACT*batch)
+    optimizer_useless=torch.optim.SGD(model.parameters(), lr=lr) 
     privacy_engine = PrivacyEngine(
         model,
         batch_size=batch,
@@ -200,12 +222,19 @@ def DiceSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
         max_grad_norm= C1,
         loss_reduction= 'sum',
         clipping_fn='Abadi')
-    # privacy_engine.attach(optimizer_useless)
+    privacy_engine.attach(optimizer_useless)
     acc_step = batch//minibatch
 
-    # optimizer=torch.optim.Adam(model.parameters(), lr = lr)
+    sigma = get_noise_multiplier(
+        target_delta=1e-5,
+        target_epsilon=2,
+        sample_rate=batch/50000,
+        epochs=epoch
+    )
+
+    optimizer=torch.optim.Adam(model.parameters(), lr = lr)
     # criterion = torch.nn.CrossEntropyLoss()
-    optimizer=torch.optim.SGD(model.parameters(), lr = lr)
+    # optimizer=torch.optim.SGD(model.parameters(), lr = lr)
     criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.1,reduction='sum')
     model.to(device)
     model.train()
@@ -229,47 +258,50 @@ def DiceSGD(model, train_dl, test_dl, batch, minibatch, epoch, C, device, lr):
             if ((t + 1) % acc_step == 0) or ((t + 1) == len(train_dl)):
                 error_norms = []
                 for param in model.parameters():
-                    if hasattr(param,'error'):
-                        first_minibatch = False
-                        error_norms.append(param.error.norm(2))
-                    else:
-                        # param.error = None
-                        first_minibatch = True
-                        error_norms.append(torch.tensor(0.))
+                    if param.requires_grad:
+                        if hasattr(param,'error'):
+                            first_minibatch = False
+                            error_norms.append(param.error.norm(2))
+                        else:
+                            # param.error = None
+                            first_minibatch = True
+                            error_norms.append(torch.tensor(0.))
                 error_norm = torch.stack(error_norms).norm(2) + 1e-6
 
                 for param in model.parameters():
-                    grad_diff=(param.grad-param.summed_clipped_grad)/batch
-                    param.grad=param.summed_clipped_grad/batch + torch.normal(
-                        mean=0,
-                        std=privacy_engine.noise_multiplier * C1/math.sqrt(batch),
-                        size=param.size(),
-                        device=device,
-                    )
-                    del param.summed_clipped_grad
-                    if first_minibatch:
-                        param.error=grad_diff
-                    else:
-                        param.grad += param.error*torch.clamp_max(C2/error_norm,1.) + torch.normal(
+                    if param.requires_grad:
+                        grad_diff=(param.grad-param.summed_clipped_grad)/batch
+                        param.grad=param.summed_clipped_grad/batch + torch.normal(
                             mean=0,
-                            std=privacy_engine.noise_multiplier * C2,
+                            std=sigma* C1,
                             size=param.size(),
                             device=device,
-                        )
-                        param.error=param.error*(1-torch.clamp_max(C2/error_norm,1.))+grad_diff
-                    del grad_diff
+                        )/batch
+                        del param.summed_clipped_grad
+                        if first_minibatch:
+                            param.error=grad_diff
+                        else:
+                            param.grad += param.error*torch.clamp_max(C2/error_norm,1.) + torch.normal(
+                                mean=0,
+                                std=sigma * C2,
+                                size=param.size(),
+                                device=device,
+                            )
+                            # param.error=param.error*(1-torch.clamp_max(C2/error_norm,1.))+grad_diff
+                            param.error=grad_diff
+                        del grad_diff
                 optimizer.step()
                 optimizer.zero_grad()
 
             if t==0 or (t+1)%PRINT_FREQ == 0 or ((t + 1) == len(train_dl)):
                 print('\rEpoch: ', E, ':', t+1, 'Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
                         % (train_loss/(t+1), 100.*correct/total, correct, total), end='')
-                correct = 0
-                total = 0
-        test(E, t, model, test_dl,device)
+                # correct = 0
+                # total = 0
+        test(E, t, model, test_dl,device,  lr, logger)
     return model
         
-def test(epoch, t, model, test_dl, device):
+def test(epoch, t, model, test_dl, device,  lr, logger):
     model.eval()
     test_loss = 0
     correct = 0
@@ -289,3 +321,4 @@ def test(epoch, t, model, test_dl, device):
     print(" ")
     print('Epoch: ', epoch, ':', t+1, 'Test Loss: %.3f | Acc: %.3f%% (%d/%d)'
             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+    logger.update([lr,epoch],[100.*correct/total, test_loss/(batch_idx+1)])
